db55551bf8e514585da04563292b8491e2d767f9
Maneesh M <maneesh.mukundan@dewa.gov.ae>
2024-05-15 11:21:46 +0400

    Added news list view

---
+++ NewsSwiftUIDemo/NewsListView.swift
@@ -0,0 +1,45 @@
+//
+//  NewsListView.swift
+//  NewsSwiftUIDemo
+//
+//  Created by Maneesh on 15/05/2024.
+//
+
+import SwiftUI
+import DEWASecrets
+
+struct NewsListView: View {
+    let newsItems: [News]
+    var dismiss: (() -> Void)?
+
+    var body: some View {
+        NavigationView {
+            
+           
+                VStack {
+                    ScrollViewReader { scrollView in
+                        List(newsItems, id: \.id) { newsItem in
+                            Text(newsItem.title)
+                        }
+                        
+                        .listStyle(.plain)
+                        .navigationBarTitle("Dewa News")
+                        .addLeadingCloseButton {
+                            
+                        }
+                    }
+                    
+                    
+                    
+                }
+            
+           
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+      
+    }
+}
+
+#Preview {
+    NewsListView(newsItems: DewaSecrets().sampleNews)
+}
