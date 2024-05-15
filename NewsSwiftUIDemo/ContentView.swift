39d6245d92be886164117f520e1c94ffc59fb267
Maneesh M <maneesh.mukundan@dewa.gov.ae>
2024-05-15 10:39:14 +0400

    Initial Commit

---
+++ NewsSwiftUIDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsSwiftUIDemo
+//
+//  Created by Maneesh on 15/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsSwiftUIDemo/NewsSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSwiftUIDemoApp.swift
+//  NewsSwiftUIDemo
+//
+//  Created by Maneesh on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
