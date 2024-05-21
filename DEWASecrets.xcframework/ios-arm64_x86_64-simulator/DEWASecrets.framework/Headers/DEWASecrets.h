56194579a07bebcdf2e7ad9a4473304d5e2e58db
Maneesh M <maneesh.mukundan@dewa.gov.ae>
2024-05-21 12:04:52 +0400

    Added DEWA Secrets Framework

---
+++ DEWASecrets.xcframework/ios-arm64_x86_64-simulator/DEWASecrets.framework/Headers/DEWASecrets.h
@@ -0,0 +1,18 @@
+//
+//  DEWASecrets.h
+//  DEWASecrets
+//
+//  Created by Maneesh on 10/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DEWASecrets.
+FOUNDATION_EXPORT double DEWASecretsVersionNumber;
+
+//! Project version string for DEWASecrets.
+FOUNDATION_EXPORT const unsigned char DEWASecretsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DEWASecrets/PublicHeader.h>
+
+
